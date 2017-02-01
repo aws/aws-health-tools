c70b55b27288d8b6b7eb3824bf6e2ff2ba366cd3
Daniele Stroppa <stroppad@amazon.com>
2017-02-01 11:43:34 -0800

    Switch phone number to Environment Variable (instead of const)

--- sms-notifier/LambdaFunction.js
+++ sms-notifier/LambdaFunction.js
@@ -2,17 +2,16 @@
 var AWS = require('aws-sdk');
 var sns = new AWS.SNS();
 
-// define configuration
-const phoneNumber =''; // Insert phone number here. For example, a U.S. phone number in E.164 format would appear as +1XXX5550100
-
 //main function which gets AWS Health data from Cloudwatch event
 exports.handler = (event, context, callback) => {
+    //get phone number from Env Variable
+    var phoneNumber = process.env.PHONE_NUMBER;
     //extract details from Cloudwatch event
     eventName = event.detail.eventTypeCode
     healthMessage = 'The following AWS Health event type has occured: ' + eventName + ' For more details, please see https://phd.aws.amazon.com/phd/home?region=us-east-1#/dashboard/open-issues';
     //prepare message for SNS to publish
     var snsPublishParams = {
-        Message: healthMessage, 
+        Message: healthMessage,
         PhoneNumber: phoneNumber,
     };
     sns.publish(snsPublishParams, function(err, data) {
@@ -20,7 +19,7 @@ exports.handler = (event, context, callback) => {
         const snsPublishErrorMessage = `Error publishing AWS Health event to SNS`;
         console.log(snsPublishErrorMessage, err);
         callback(snsPublishErrorMessage);
-        } 
+        }
     else {
         const snsPublishSuccessMessage = `Successfully got details from AWS Health event, ${eventName} and sent SMS via SNS.`;
         console.log(snsPublishSuccessMessage, data);
@@ -28,4 +27,3 @@ exports.handler = (event, context, callback) => {
         }
     });
 };
-
