d7f25c8deb945a7a721d0611101703faf4ef6c63
Jay McConnell <jmmccon@amazon.com>
2017-02-07 06:22:45 +0200

    * create cfn template * update readme * updated function code:     * use environment variables for config     * cater for both stop and terminate actions     * handle dry-run exception     * improve readibility

--- automated-actions/AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED/LambdaFunction.js
+++ automated-actions/AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED/LambdaFunction.js
@@ -2,40 +2,96 @@
 var AWS = require('aws-sdk');
 
 // define configuration
-const tagKey ='stage';
-const tagValue ='prod';
+const tagKey = process.env.TAG_KEY;
+const tagValue = process.env.TAG_VALUE;
+const action = process.env.EC2_ACTION;
+const dryRun = process.env.DRY_RUN;
 
-//main function which gets AWS Health data from Cloudwatch event
-exports.handler = (event, context, callback) => {
-    //extract details from Cloudwatch event
-    eventName = event.detail.eventTypeCode;
-    affectedEntities = event.detail.affectedEntities;
-    region = event.region;
-    const awsHealthSuccessMessage = `Successfully got details from AWS Health event, ${eventName} and executed automated action.`;
-    
-    // setting up a loop that calls the function for the automated action for each of the resources flagged by AWS Health
+function getMatchingInstances(affectedEntities){
+    //initialize an empty array
+    var instances = [];
+    // loop through entities
     for ( var i=0; i < affectedEntities.length; i+=1 )
     {
-        instanceId = affectedEntities[i].entityValue;
+        var instanceId = affectedEntities[i].entityValue;
+        // check that tags match
         if (affectedEntities[i].tags[[tagKey]] == tagValue){
-            stopInstances (instanceId, region);
+            // add instanceid to the array
+            instances.push(instanceId);
         }
         else console.log ('The following instance does not match the configured tag: ', instanceId);
     }
-    callback(null, awsHealthSuccessMessage); //return success
-};
-
-//This function stops an EC2 Instance
-function stopInstances (instanceId, region) {
-    AWS.config.update({region: region});
-    var ec2 = new AWS.EC2();
-    console.log ('attempting to stop the following instance: ', instanceId);
-    var stopInstancesParams = {
-        InstanceIds: [instanceId],
-        DryRun: true
-    };
-    ec2.stopInstances(stopInstancesParams, function(err, data) {
-        if (err) console.log(instanceId, region, err, err.stack); // an error occurred
-        else console.log("Instance stopped: ", instanceId, region);           // successful response
-    });
+    return instances
 }
+
+function setupClient(region){
+    // set the region for the sdk
+    AWS.config.update({region: region});
+    //create the ec2 client
+    return new AWS.EC2();
+}
+
+function getParams(instances, dryRun){
+    // setup parameters
+    var instancesParams = {
+        InstanceIds: instances,
+        DryRun: false
+    };
+    // enable DryRun if set in environment variables
+    if (dryRun == 'true')  {
+        instancesParams.DryRun = true;
+        console.log()
+    }
+    return instancesParams
+}
+
+//main function which gets AWS Health data from Cloudwatch event
+exports.handler = (event, context, callback) => {
+
+    // function to handle ec2 API response
+    function handleResponse(err, data) {
+        if (err) {                                                          // an error occurred
+            if (err.code == 'DryRunOperation') {
+                console.log(instances, region, err.message);
+                callback(null, awsHealthSuccessMessage);
+            } else {
+                console.log(instances, region, err, err.stack);
+                throw err;
+            }
+
+        } else {
+            console.log(`Instance ${action}: `, instances, region);
+            //return success
+            callback(null, awsHealthSuccessMessage);
+        }                                                                   // successful response
+    }
+
+    //extract details from Cloudwatch event
+    var eventName = event.detail.eventTypeCode;
+    var affectedEntities = event.detail.affectedEntities;
+    var region = event.region;
+
+    const awsHealthSuccessMessage = `Successfully got details from AWS Health event, ${eventName} and executed automated action.`;
+
+    // get affected instances that match the required tags
+    instances = getMatchingInstances(affectedEntities);
+
+    if (instances.length > 0) {                           //there are some instances to take action on
+
+        //create an ec2 api client in the event's region
+        var ec2 = setupClient(region);
+
+        // setup parameters
+        var instancesParams = getParams(instances, dryRun);
+
+        console.log (`attempting to ${action} the following instances: `, instances);
+        // Call either the Terminate or the Stop API
+        if (action == 'Terminate') ec2.terminateInstances(instancesParams, handleResponse);
+        else ec2.stopInstances(instancesParams, handleResponse);
+
+    } else {
+        console.log("No instances in the event match the required tags, exiting without any action");
+        callback(null, awsHealthSuccessMessage);
+    }
+
+};
--- automated-actions/AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED/README.md
+++ automated-actions/AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED/README.md
@@ -4,13 +4,30 @@
 EC2 has detected a performance degradation of one or more physical storage drives that backs the instance store volumes of your Amazon EC2 instance. Because of this degradation, some instance store volumes could be unresponsive or exhibit poor performance.
 
 ### Setup and Usage
-You can automatically stop or terminate EC2 instances that have degraded instance-store performance using Amazon Cloudwatch events and AWS Lambda using the following instructions:
+You can automatically stop or terminate EC2 instances that have degraded instance-store performance using Amazon Cloudwatch events and AWS Lambda.
+
+#### CloudFormation
+Choose **Launch Stack** to launch the AWS Health SMS Notifier template in the US East (N. Virginia) Region in your account:
+
+[![Launch AWS Health Automated Action](../images/cloudformation-launch-stack.png)](https://console.aws.amazon.com/cloudformation/home?region=us-east-1#/stacks/new?stackName=SmsNotifier&templateURL=https://s3.amazonaws.com/aws-health-tools/Cloudformation-templates/aa-instance-store-degraded.json)
+
+The CloudFormation template requires the following parameters:
+
+*Action to take* - Whether to Stop or Terminate affected instances
+
+*Tag Key* - Instances must have a matching tag key and value, this is the key to match
+
+*Tag Value* - the tag value to match
+
+*Dry Run* - Set to true for testing. setting this to true will run the requested EC2 API in DryRun mode, not actually stopping/terminating instances
+
+#### Manual setup
 
 1. Create an IAM role for the Lambda function to use. Attach the [IAM policy](IAMPolicy) to the role in the IAM console.
 Documentation on how to create an IAM policy is available here: http://docs.aws.amazon.com/IAM/latest/UserGuide/access_policies_create.html
 Documentation on how to create an IAM role for Lambda is available here: http://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_create_for-service.html#roles-creatingrole-service-console
 
-2. Create a Lambda JavaScript function by using the [sample](LambdaFunction.js) provided and choose the IAM role created in step 1. The sample Lambda function will stop EC2 instances when AWS Health AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED events are generated. This is useful for situations where there is data redundancy and fault tolerance (for example, when using Auto Scaling).  Be sure to set the appropriate tag key and value in the configuration section of the Lambda function. 
+2. Create a Lambda JavaScript function by using the [sample](LambdaFunction.js) provided and choose the IAM role created in step 1. The sample Lambda function will stop EC2 instances when AWS Health AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED events are generated. This is useful for situations where there is data redundancy and fault tolerance (for example, when using Auto Scaling).  Be sure to set the appropriate tags and region in the configuration section of the Lambda function.
 More information about Lambda is available here: http://docs.aws.amazon.com/lambda/latest/dg/getting-started.html
 
 3. Create a CloudWatch Events rule to trigger the Lambda function created in step 2 matching the AWS_EC2_INSTANCE_STORE_DRIVE_PERFORMANCE_DEGRADED event.
@@ -18,8 +35,7 @@ Documentation on how to create an AWS Health CloudWatch Events rule is available
 
 More information about AWS Health is available here: http://docs.aws.amazon.com/health/latest/ug/what-is-aws-health.html
 
-Note that this is a just an example of how to set up automation with AWS Health, Amazon CloudWatch Events, and AWS Lambda. We recommend testing the example and tailoring it to your environment before using it in your production environment. 
+Note that this is a just an example of how to set up automation with AWS Health, Amazon CloudWatch Events, and AWS Lambda. We recommend testing the example and tailoring it to your environment before using it in your production environment.
 
 ### License
 AWS Health Tools are licensed under the Apache 2.0 License.
-
