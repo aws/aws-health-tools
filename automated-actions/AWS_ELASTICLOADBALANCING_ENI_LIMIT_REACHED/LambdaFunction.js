d8a76e7cbbb0bf19509b40e8848872388ab80844
mwpatrick <mwpatrickgithub@gmail.com>
2017-02-17 19:18:49 +1100

    Updates, including a CF template - testing needed

--- automated-actions/AWS_ELASTICLOADBALANCING_ENI_LIMIT_REACHED/LambdaFunction.js
+++ automated-actions/AWS_ELASTICLOADBALANCING_ENI_LIMIT_REACHED/LambdaFunction.js
@@ -2,6 +2,7 @@
 // This is useful for situations where you might have leftover ENIs that are not used and are preventing load balancer scaling
 'use strict';
 var AWS = require('aws-sdk');
+const dryRun = process.env.DRY_RUN || 'true';
 
 //main function which gets AWS Health data from Cloudwatch event
 exports.handler = (event, context, callback) => {
@@ -10,8 +11,7 @@ exports.handler = (event, context, callback) => {
     var region = event.region;
     const awsHealthSuccessMessage = `Successfully got details from AWS Health event, ${eventName} and executed automated action.`;
 
-	// the event could have multiple load balancers listed but this does not matter here
-	// ultimately we only need to run this automation once per invocation since the issue 
+	// we only need to run this automation once per invocation since the issue 
 	// of ENI exhaustion is regional and not dependent on the load balancers in the alert
 	// Event will only trigger for one region so we don't have to loop that
 	AWS.config.update({region: region});
@@ -22,35 +22,42 @@ exports.handler = (event, context, callback) => {
 		Filters: [{	Name: 'status',	Values: ['available']}]
 	};
 	
-	var networkInterfaces = null;
 	ec2.describeNetworkInterfaces(params, function(err, data) {
 		if (err) console.log( region, err, err.stack); // an error occurred
 		else 
 		{
-			console.log("Found %s available ENI",data.NetworkInterfaces.length); // successful response
+			console.log('Found %s available ENI',data.NetworkInterfaces.length); // successful response
 			// for each interface, remove it
-			for ( var i=0; i < data.NetworkInterfaces.length; i+=1) deleteNetworkInterface(data.NetworkInterfaces[i], region);
-		}
-	});
+			for ( var i=0; i < data.NetworkInterfaces.length; i+=1)
+			{
+				if (dryRun == 'true')
+				{
+					console.log('Dry run is true - not deleting %s', data.NetworkInterfaces[i]);			
+				} else {
+					console.log('No dry run - considering deletions');
+					for ( var i=0; i < len; i+=1) { 
+						deleteNetworkInterface(data.NetworkInterfaces[i]); 
+					}
+				}
+			}
+		});
 
     callback(null, awsHealthSuccessMessage); //return success
 };
 
 //This function removes an available (unattached) ENI
-//Take an instance description as argument so we can verify attachment status
-function deleteNetworkInterface (networkInterface, region) {
-    AWS.config.update({region: region});
+function deleteNetworkInterface (networkInterface) {
     var ec2 = new AWS.EC2();
 	
-	if (networkInterface.Status == "available") {
+	if (networkInterface.Status == 'available') {
 		console.log ('Attempting to delete the following ENI: %s', networkInterface.NetworkInterfaceId);
 		var deleteNetworkInterfaceParams = {
 			NetworkInterfaceId: networkInterface.NetworkInterfaceId,
 			DryRun: false
 		};
 		ec2.deleteNetworkInterface(deleteNetworkInterfaceParams, function(err, data) {
-			if (err) console.log(networkInterface.NetworkInterfaceId, region, err, err.stack); // an error occurred
-			else console.log("ENI deleted: %s", networkInterface.NetworkInterfaceId);           // successful response
+			if (err) console.log(networkInterface.NetworkInterfaceId, err, err.stack); // an error occurred
+			else console.log('ENI deleted: %s', networkInterface.NetworkInterfaceId);           // successful response
 		});
 	}
 	else console.log ('The following ENI is not in an available (unattached) state: %s', networkInterface.NetworkInterfaceId);
