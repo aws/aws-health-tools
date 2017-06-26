646305e89a5d09f60ce9ccb0b56efe81a15d633e
Rob Percival <robperc@amazon.com>
2017-06-25 20:23:23 -0700

    Initial commit

---
+++ automated-actions/AWS_RISK_CREDENTIALS_EXPOSED/lambda_functions/delete_access_key_pair.py
@@ -0,0 +1,44 @@
+import boto3
+
+iam = boto3.client('iam')
+
+
+def lambda_handler(event, context):
+    account_id = event['account']
+    time_discovered = event['time']
+    details = event['detail']
+    access_key_id = details['affectedEntities'][0]['entityValue']
+    print('Looking up username for access key pair...')
+    username = get_username_from_key(access_key_id)
+    print('Deleting exposed access key pair...')
+    delete_exposed_key_pair(username, access_key_id)
+    return {
+        "account_id": account_id,
+        "time_discovered": time_discovered,
+        "username": username,
+        "deleted_key": access_key_id
+    }
+
+
+def get_username_from_key(access_key_id):
+    try:
+        response = iam.get_access_key_last_used(
+            AccessKeyId=access_key_id
+        )
+    except Exception as e:
+        print(e)
+        print('Unable to retrieve username for access key "{}".'.format(access_key_id))
+        raise(e)
+    return response['UserName']
+
+
+def delete_exposed_key_pair(username, access_key_id):
+    try:
+        iam.delete_access_key(
+            UserName=username,
+            AccessKeyId=access_key_id
+        )
+    except Exception as e:
+        print(e)
+        print('Unable to delete access key "{}" for user "{}".'.format(access_key_id, username))
+        raise(e)
