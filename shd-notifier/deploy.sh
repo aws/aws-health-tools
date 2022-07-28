c0e8d8d1142782f3efca2b0376f207409412fe49
greyvugrin <grey@the-atlas.com>
2022-07-28 13:21:06 -0700

    Fix issue with AWS cli v2 hanging on user input

--- shd-notifier/deploy.sh
+++ shd-notifier/deploy.sh
@@ -11,12 +11,22 @@ if [ $# -lt 2 ]; then
   echo 1>&2 "  REGION: The region of the deployed CloudFormation template"
   exit 2
 fi
+
+AWS_VERSION=$(aws --version)
+AWS_COMMAND_FLAGS=""
+
+# check if aws version is 2.x
+# we need to stop the command from waiting for user input
+if [[ $AWS_VERSION == *"aws-cli/2"* ]]; then
+  AWS_COMMAND_FLAGS="--no-cli-pager"
+fi
+
 createOrUpdate () {
     FUNC_NAME=$1
     SOURCE=$2
     cp "$SOURCE" index.py
     zip "$FUNC_NAME.zip" index.py
-    aws lambda update-function-code --function-name "$FUNC_NAME" --zip-file "fileb://$FUNC_NAME.zip" --region "$REGION"
+    aws lambda update-function-code --function-name "$FUNC_NAME" --zip-file "fileb://$FUNC_NAME.zip" --region "$REGION" $AWS_COMMAND_FLAGS
     rm "$FUNC_NAME.zip"
     rm index.py
 }
