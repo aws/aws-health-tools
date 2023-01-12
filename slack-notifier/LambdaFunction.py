bae9c78b4d8bcfc3ced42fb234e9581a018a8862
borja.tur <borja.tur@screeningeagle.com>
2023-01-12 16:53:26 +0100

    source code aligned with cfn templates

--- slack-notifier/LambdaFunction.py
+++ slack-notifier/LambdaFunction.py
@@ -1,5 +1,5 @@
 '''
-This is a sample function to send AWS Health event messages to a Slack channel. 
+This is a sample function to send AWS Health event messages to a Slack channel.
 
 Follow these steps to configure the webhook in Slack:
 
@@ -15,42 +15,48 @@ You can also use KMS to encrypt the webhook URL as shown here: https://aws.amazo
 '''
 
 from __future__ import print_function
-
 import boto3
 import json
 import logging
 import os
-
-from urllib2 import Request, urlopen, URLError, HTTPError
+from urllib.request import Request, urlopen, URLError, HTTPError
+from urllib.parse import urlencode
 
 #configuration
 
 # The Slack channel to send a message to stored in the slackChannel environment variable
-SLACK_CHANNEL = "#awshealth"
+SLACK_CHANNEL = '#awshealth'
 # Add the webhook URL from Slack below
-HOOK_URL = "https://hooks.slack.com/services/example"
+HOOK_URL = 'https://hooks.slack.com/services/example'
 # Setting up logging
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-
-#main function
-
-def lambda_handler(event, context):
-    message =  str(event['detail']['eventDescription'][0]['latestDescription']  + "\n\n<https://phd.aws.amazon.com/phd/home?region=us-east-1#/event-log?eventID=" + event['detail']['eventArn'] + "|Click here> for details.")
+# main function
+def handler(event, context):
+    message =  str(
+        event['detail']['eventDescription'][0]['latestDescription']  +
+        '\n\n<https://phd.aws.amazon.com/phd/home?region=us-east-1#/event-log?eventID=' +
+        event['detail']['eventArn'] +
+        '|Click here> for details.'
+    )
     json.dumps(message)
     slack_message = {
         'channel': SLACK_CHANNEL,
-        'text': message
+        'text': message,
+        'username': 'AWS - Personal Health Updates'
     }
     logger.info(str(slack_message))
-    
-    req = Request(HOOK_URL, json.dumps(slack_message))
+    req = Request(
+        HOOK_URL,
+        data=json.dumps(slack_message).encode('utf-8'),
+        headers={'content-type': 'application/json'}
+    )
     try:
         response = urlopen(req)
         response.read()
-        logger.info("Message posted to %s", slack_message['channel'])
+        logger.info('Message posted to: %s', slack_message['channel'])
     except HTTPError as e:
-        logger.error("Request failed: %d %s", e.code, e.reason)
+        logger.error('Request failed : %d %s', e.code, e.reason)
     except URLError as e:
-        logger.error("Server connection failed: %s", e.reason)
+        logger.error('Server connection failed: %s', e.reason)
 
